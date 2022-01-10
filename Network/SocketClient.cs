83464a77f004d791c3d6ebdfa638d15a1c96b73c
Giel Jurriëns <gieldid@gmail.com>
2022-01-10 20:13:27 +0100

    Added socket connection

---
+++ Network/SocketClient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using DatawoodGH.Network;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace CSVModule.Network
+{
+    public class SocketClient : NetworkComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the WebSocketComponent class.
+        /// </summary>
+        public SocketClient()
+          : base("Socket Client", "Socket",
+              "Makes a socket client and connects to given server.")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Ip", "IP", "IP to make a web socket connection to", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Port","P","Port for the socket connection",GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Run", "R", "When to run", GH_ParamAccess.item, true);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Message","m","Message received",GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string ip = null;
+            int port = 0;
+            bool run = true;
+
+
+            if (!DA.GetData(0, ref ip)) {
+                return;
+            }
+
+            if (!DA.GetData(1, ref port)) {
+                return;
+            }
+
+            DA.GetData(2, ref run);
+            
+            if (run) {
+                SocketConnection(ip, port);
+
+            }
+        }
+
+        private void SocketConnection(string ip, int port) {
+            byte[] bytes = new byte[1024];
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(ip);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+            Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            client.Connect(remoteEP);
+            this.Message = "Socket connected to "+ client.RemoteEndPoint.ToString();
+     
+            byte[] payload = Encoding.UTF8.GetBytes("listening?");
+            client.Send(payload);
+
+            int bytesRec = client.Receive(bytes);
+            this.Message = "Received the following: " + Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("50843146-2808-4E45-8501-6716BDACB6EC"); }
+        }
+    }
+}
